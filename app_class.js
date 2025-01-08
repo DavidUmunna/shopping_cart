a29159641e65f419df75669987a80aea585da8a9
chima1968 <chimarokeumunna98@gmail.com>
2025-01-08 16:41:09 +0100

    refactor complete

--- app_class.js
+++ app_class.js
@@ -15,7 +15,7 @@ class shopping_cart{
         this.product=document.querySelectorAll('.item')
         let total=0 
     
-        items.forEach((item) =>{
+        this.product.forEach((item) =>{
             const priceElement = item.querySelector('.item-price').getAttribute('data-price');
             this.product.price=parseFloat(priceElement)
             const quantityElement=item.querySelector('.quantity').textContent;
@@ -40,14 +40,15 @@ class shopping_cart_item extends shopping_cart{
         add_buttons.forEach((add_button) => {
             
             let quantity_display = add_button.closest('.item').querySelector('.quantity');
-            
+            let product = add_button.closest('.item').getAttribute('data-product');
+            let cartItem= new shopping_cart_item(product)
             const updatequantity = () => {
-                quantity_display.textContent = quantity;
-                updatetotalprice()
+                quantity_display.textContent = cartItem.quantity;
+                this.updatetotalprice()
             };
             
             add_button.addEventListener('click', () => {
-                quantity++;
+                cartItem.quantity++;
                 updatequantity();
             });
             
@@ -56,9 +57,55 @@ class shopping_cart_item extends shopping_cart{
         
     
     }
-    remove_item(){
-        if(this.quantity>0){
-            this.quantity--
-        }
+    minus_items(){
+        let minus_button = document.querySelectorAll('.minus');
+        
+        minus_button.forEach((minus_button) => {
+            
+            let quantity_display = minus_button.closest('.item').querySelector('.quantity');
+            let product = minus_button.closest('.item').getAttribute('data-product');
+            let cartItem= new shopping_cart_item(product)
+            const updatequantity = () => {
+                quantity_display.textContent = cartItem.quantity;
+                this.updatetotalprice()
+            };
+            
+            minus_button.addEventListener('click', () => {
+                if (cartItem.quantity > 0) {
+                    cartItem.quantity--;
+                }
+                updatequantity();
+            });
+        });
+    }
+
+    likes(){
+        let heart=document.querySelectorAll('.heart')
+        heart.forEach((heart)=>{
+            heart.addEventListener('click',()=>{
+                heart.style.color=heart.style.color==='red'? "black":"red"
+            })
+        });
+    }
+
+    delete_card(){
+        let trash=document.querySelectorAll('.trash')
+        trash.forEach((trash)=>{
+            trash.addEventListener('click',()=>{
+                let card = trash.closest('.item')
+                if (card){
+                    card.remove();
+                }
+            })
+        })
     }
 }                                               
+
+let shopping_class=new shopping_cart_item()
+shopping_class.add_items()
+shopping_class.minus_items()
+shopping_class.likes()
+shopping_class.delete_card()
+
+let total_upodate=new shopping_cart()
+total_upodate.updatetotalprice()
